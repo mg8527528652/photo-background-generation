c07cb16b041a6a52aba3f51b628ba808a6c592d0
rishabh <rishabh.srivastava@phot.ai>
2024-11-20 07:18:24 +0530

    test

--- train_controlnet.py
+++ train_controlnet.py
@@ -387,7 +387,7 @@ def parse_args(input_args=None):
     parser.add_argument(
         "--dataloader_num_workers",
         type=int,
-        default=4,
+        default=1,
         help=(
             "Number of subprocesses to use for data loading. 0 means that the data will be loaded in the main process."
         ),
